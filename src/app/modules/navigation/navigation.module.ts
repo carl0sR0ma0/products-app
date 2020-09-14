66a2b2220426fb2ac580d9a23d8855e5aa521531
carl0sR0ma0 <cvpromao@gmail.com>
2020-09-14 15:41:54 -0300

    Implementação inicial do componente Navigation

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+/**Importações do Material */
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { NavigationComponent } from './navigation.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [
+    NavigationComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule    
   ],
   exports: [
     NavigationComponent
