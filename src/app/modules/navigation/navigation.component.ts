b59466470f693cf8fe1f94e5a3319ad04794fe2a
carl0sR0ma0 <cvpromao@gmail.com>
2020-09-21 13:49:58 -0300

    Refatorando a tela principal, colocando um icone e uma imagem.

--- src/app/modules/navigation/navigation.component.ts
+++ src/app/modules/navigation/navigation.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  title: string = 'Ibicity Market'
+  title: string = 'Ibicity'
 
   constructor() { }
 
